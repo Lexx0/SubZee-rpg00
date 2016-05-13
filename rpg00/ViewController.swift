243d398bc1fe9dfb19bf821e9622d0ea7effa3dd
Alex Berezovsky <zoid@mbp-aberezovsky.lan>
2016-05-13 16:22:13 +0300

    Initial Commit

---
+++ rpg00/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rpg00
+//
+//  Created by Alex Berezovsky on 5/13/16.
+//  Copyright © 2016 PatataMatata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
